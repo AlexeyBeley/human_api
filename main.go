566e7d4642d49553451813e57061ee92120d494a
horey <alexey.beley@gmail.com>
2025-02-22 10:01:44 -0600

    Added file

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func NotMain() {
+	/*
+		   daily_handler --action daily_json_to_hr --src --dst
+
+		action := flag.String("action", "none", "The action to take")
+
+		flag.Parse()
+
+		if *action == "daily" {
+			log.Println("daily")
+		} else if *action == "hr_to_daily_json" {
+			log.Println("daily")
+		} else {
+			log.Fatalf("Unknown action '%v'", *action)
+		}
+	*/
+	fmt.Print("here")
+}
